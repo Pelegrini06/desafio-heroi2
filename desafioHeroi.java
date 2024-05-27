e4fb515b759afbafbd3e5189da9fea70d06bf41a
Pelegrini06 <leandropelegrini06@gmail.com>
2024-05-27 20:38:19 -0300

    if/else

---
+++ desafioHeroi.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class desafioHeroi{
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+
+    int vitoria = scanner.nextInt();
+    System.out.println("Você tem " + vitoria + " vitórias!");
+
+    int derrota = scanner.nextInt();   
+    System.out.println("Você tem " + derrota + " derrotas!");
+
+    int ranked = vitoria - derrota;
+
+   if (ranked <= 10) {
+        System.out.println("O Herói tem de saldo de " + vitoria + "está no nível de Ferro");
+   }else if (ranked >=11 && ranked <=20) {
+        System.out.println("O Herói tem de saldo de " + vitoria + "está no nível de Bronze");
+   }else if (ranked >=21 && ranked <=50) {
+        System.out.println("O Herói tem de saldo de " + vitoria + "está no nível de Prata");
+   }else if (ranked >=51 && ranked<=80) {
+        System.out.println("O Herói tem de saldo de " + vitoria + "está no nível de Ouro");
+   }else if (ranked >=81 && ranked <=90) {
+        System.out.println("O Herói tem de saldo de " + vitoria + "está no nível de Diamante");
+   }else if (ranked >=91 && ranked <=100) {
+        System.out.println("O Herói tem de saldo de " + vitoria + "está no nível de Lendário");
+   }else {
+        System.out.println("O Herói tem de saldo de " + vitoria + "está no nível de Imortal");
+   }
+
+    scanner.close();
+    System.out.println();
+}
+}
+
